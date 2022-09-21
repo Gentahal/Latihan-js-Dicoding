fa6475e4e8465f277af1ab110892c650ebecd4a9
Rifky Syaripudin <rks@jiwalu.id>
2022-09-21 08:40:37 +0700

    commit again

--- main.js
+++ main.js
@@ -21,13 +21,20 @@
 // }
 
 //!If else
-function scoreChecker(score) {
-    let result;
+let score = 100;
+let result = ""
 
-    if (score >= 90) {
-        result='Selamat!! Anda mendapatkan nilai A'
-    }
-
-    return result;
-
+if(score >= 90) {
+    result = "Selamat! Anda mendapatkan nilai A."
+} else if(score >= 80) {
+    result = "Anda mendapatkan nilai B."
+} else if(score >= 70) {
+    result = "Anda mendapatkan nilai C."
+} else if(score >= 60) {
+    result = "Anda mendapatkan nilai D."
+}  else if(score <= 60) {
+    result = "Anda mendapatkan nilai E."
 }
+
+
+console.log(result);
