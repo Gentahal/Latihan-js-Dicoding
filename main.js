3c5a95beda484fea0f3d420a0ffbd8bd97932bdc
Rifky Syaripudin <rks@jiwalu.id>
2022-09-21 08:00:19 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,33 @@
+//!!For of/
+// let myArray = ['Genta', 'Halilintar']
+
+// for(const arrayItem of myArray) {
+//     console.log()
+// }
+
+//!Do While/
+// let i= 0;
+
+// do {
+//     console.log();
+//     i++
+// } while (i <= 100);
+
+//!infinite loops /
+// let a = 1;
+
+// while(a <= 5) {
+//     console.log()
+// }
+
+//!If else
+function scoreChecker(score) {
+    let result;
+
+    if (score >= 90) {
+        result='Selamat!! Anda mendapatkan nilai A'
+    }
+
+    return result;
+
+}
